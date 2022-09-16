b2a5bf224653c922df11eae439bdc9893700a6b1
Peter Koomen <koomen@gmail.com>
2022-09-15 18:02:27 -0700

    Use mutation observer to wait for shop now button to exist

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,27 @@
+// Lifted from Yong Wang's answer on 
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 window.fullStackOnReady = () => {
-    document.querySelector(".btn.hero__cta").style.backgroundColor = "red";
+    waitForElm(".btn.hero__cta").then((elm) => {
+        elm.style.backgroundColor = "red";
+    });
 }
